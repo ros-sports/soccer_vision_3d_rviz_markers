68b4b637a6b8362a0db75801eeee2d2d204aa1fe
Kenji Brameld <kenjibrameld@gmail.com>
2022-06-29 17:31:27 +0900

    update package description and license in setup.py
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ setup(
     zip_safe=True,
     maintainer='ijnek',
     maintainer_email='kenjibrameld@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Package that converts soccer_vision_3d_msgs to RViz markers',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
